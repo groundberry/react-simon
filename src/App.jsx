6f8545faf11e2651f5298c3e197b1b62ceef2c2a
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-05-31 20:43:40 -0700

    Add enzyme to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Simon says</h2>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Simon says</h2>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
